d63769752da907408fd265d7cd69c02aa63c68df
Vidra13 <vidricradivoj@gmail.com>
2025-10-22 18:51:46 +0200

    Initial commit: osnovna struktura projekta i main + saberi

---
+++ src/kalkulator.c
@@ -0,0 +1,6 @@
+#include "kalkulator.h"
+
+int saberi(int a, int b) { return a + b; }
+int oduzmi(int a, int b) { return a - b; }
+int pomnozi(int a, int b) { return a * b; }
+float podeli(int a, int b) { return (float)a / b; }
---
+++ src/kalkulator.h
@@ -0,0 +1,9 @@
+#ifndef KALKULATOR_H
+#define KALKULATOR_H
+
+int saberi(int a, int b);
+int oduzmi(int a, int b);
+int pomnozi(int a, int b);
+float podeli(int a, int b);
+
+#endif
