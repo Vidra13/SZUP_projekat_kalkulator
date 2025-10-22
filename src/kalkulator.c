75b3b7aa3f2c43c0b7a70b5ebd2fc9b9e29b5e9c
Vidra13 <vidricradivoj@gmail.com>
2025-10-22 19:01:04 +0200

    Popravka: dodata provera deljenja nulom u podeli

--- src/kalkulator.c
+++ src/kalkulator.c
@@ -3,4 +3,9 @@
 int saberi(int a, int b) { return a + b; }
 int oduzmi(int a, int b) { return a - b; }
 int pomnozi(int a, int b) { return a * b; }
-float podeli(int a, int b) { return (float)a / b; }
+
+/* popravka: dodatna sigurnost deljenja nulom */
+float podeli(int a, int b) {
+    if (b == 0) return 0.0f;
+    return (float)a / b;
+}
