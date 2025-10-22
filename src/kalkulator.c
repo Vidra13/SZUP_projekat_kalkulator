8a0c423df640f74972405e1da351220ebff31f0c
Vidra13 <vidricradivoj@gmail.com>
2025-10-22 19:00:23 +0200

    Dodavanje funkcija za mnozenje i deljenje

--- src/kalkulator.c
+++ src/kalkulator.c
@@ -2,3 +2,5 @@
 
 int saberi(int a, int b) { return a + b; }
 int oduzmi(int a, int b) { return a - b; }
+int pomnozi(int a, int b) { return a * b; }
+float podeli(int a, int b) { return (float)a / b; }
